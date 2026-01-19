64cb6eaa33c0f027014266bee0fc4f446419a174
GoJunbeom <gojunbeom430@gmail.com>
2026-01-19 15:12:21 +0900

    refactor: 3 Layer- controller- Service-Repository 계층 분리

---
+++ src/main/java/com/meta/memo/controller/MemoController.java
@@ -0,0 +1,45 @@
+package com.meta.memo.controller;
+
+import com.meta.memo.dto.MemoResponseDto;
+import com.meta.memo.dto.MemoRequestDto;
+import com.meta.memo.service.MemoService;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/memos")
+public class MemoController {
+    // JDBC를 통한 MySQL 연결
+    private final JdbcTemplate jdbcTemplate;
+
+    public MemoController(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @PostMapping()
+    public MemoResponseDto createMemo(@RequestBody MemoRequestDto memoRequestDto) {
+        MemoService memoService = new MemoService(jdbcTemplate);
+        return memoService.createMemo(memoRequestDto);
+    }
+
+    @GetMapping()
+    public List<MemoResponseDto> getMemos() {
+        MemoService memoService = new MemoService(jdbcTemplate);
+        return memoService.getMemos();
+    }
+
+
+    @PutMapping("{id}")
+    public Long updateMemo(@PathVariable Long id, @RequestBody MemoRequestDto memoRequestDto) {
+        MemoService memoService = new MemoService(jdbcTemplate);
+        return memoService.updateMemo(id, memoRequestDto);
+    }
+
+    @DeleteMapping("{id}")
+    public Long deleteMemo(@PathVariable Long id) {
+        MemoService memoService = new MemoService(jdbcTemplate);
+        return memoService.deleteMemo(id);
+    }
+}
---
+++ src/main/java/com/meta/memo/service/MemoService.java
@@ -0,0 +1,74 @@
+package com.meta.memo.service;
+
+import com.meta.memo.domain.Memo;
+import com.meta.memo.dto.MemoResponseDto;
+import com.meta.memo.dto.MemoRequestDto;
+import com.meta.memo.repository.MemoRepository;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+public class MemoService {
+    // JDBC를 통한 MySQL 데이터베이스연결
+    private final JdbcTemplate jdbcTemplate;
+
+    public MemoService(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+
+    public MemoResponseDto createMemo(@RequestBody MemoRequestDto memoRequestDto) {
+        // RequestDto -> Entity 변환
+        Memo newMemo = new Memo(memoRequestDto);
+
+        MemoRepository memoRepository = new MemoRepository(jdbcTemplate);
+        Memo savedMemo = memoRepository.save(newMemo);
+
+        // Entity -> ResponseDto 변환
+        MemoResponseDto memoResponseDto = new MemoResponseDto(savedMemo);
+
+        return memoResponseDto;
+    }
+
+    public List<MemoResponseDto> getMemos() {
+        MemoRepository memoRepository = new MemoRepository(jdbcTemplate);
+        List<MemoResponseDto> memoResponseDtoList = memoRepository.findAll();
+        return memoResponseDtoList;
+    }
+
+    public Long updateMemo(@PathVariable Long id, @RequestBody MemoRequestDto memoRequestDto) {
+        MemoRepository memoRepository = new MemoRepository(jdbcTemplate);
+        // 해당 id의 메모가 존재하는지 확인
+        Memo foundmemo = memoRepository.findById(id);
+
+        // 해당 id의 메모가 데이터베이스에 존재하는지 확인, 메모 내용 수정
+        if (foundmemo != null) {
+            Long updatedID = memoRepository.update(id, memoRequestDto);
+            return id;
+        } else {
+            throw new IllegalArgumentException("선택한 id의 메모는 존재하지 않습니다.");
+        }
+    }
+
+    public Long deleteMemo(@PathVariable Long id) {
+        // 해당 id의 메모가 존재하는지 확인
+        Memo foundmemo = MemoRepository.findById(id);
+
+        // 메모 내용 삭제
+        if (foundmemo != null){
+            Long deletedID = MemoRepository.delete(id);
+            return deletedID;
+
+        } else {
+            throw new IllegalArgumentException("선택한 id의 메모는 존재하지 않습니다.");
+        }
+    }
+
+}
