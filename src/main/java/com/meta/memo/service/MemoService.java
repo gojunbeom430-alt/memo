869292d60caec3e32eec61248c2cc23cef0752f2
GoJunbeom <gojunbeom430@gmail.com>
2026-01-21 12:50:44 +0900

    refactor: JPA 상속을 통한 Repository로 변경

--- src/main/java/com/meta/memo/service/MemoService.java
+++ src/main/java/com/meta/memo/service/MemoService.java
@@ -30,45 +30,37 @@ public class MemoService {
     public MemoResponseDto createMemo(@RequestBody MemoRequestDto memoRequestDto) {
         // RequestDto -> Entity 변환
         Memo newMemo = new Memo(memoRequestDto);
-
         Memo savedMemo = memoRepository.save(newMemo);
-
         // Entity -> ResponseDto 변환
         MemoResponseDto memoResponseDto = new MemoResponseDto(savedMemo);
-
         return memoResponseDto;
     }
 
     public List<MemoResponseDto> getMemos() {
-        List<MemoResponseDto> memoResponseDtoList = memoRepository.findAll();
+        List<MemoResponseDto> memoResponseDtoList = memoRepository.findAll().stream()
+                .map(MemoResponseDto::new).toList();
         return memoResponseDtoList;
+    }
+
+    public Memo getMemoByID(Long id) {
+        return memoRepository.findById(id).orElseThrow(()->
+                new IllegalArgumentException("선택한 id의 메모는 존재하지 않습니다."));
     }
 
     public Long updateMemo(@PathVariable Long id, @RequestBody MemoRequestDto memoRequestDto) {
         // 해당 id의 메모가 존재하는지 확인
-        Memo foundmemo = memoRepository.findById(id);
-
-        // 해당 id의 메모가 데이터베이스에 존재하는지 확인, 메모 내용 수정
-        if (foundmemo != null) {
-            Long updatedID = memoRepository.update(id, memoRequestDto);
-            return id;
-        } else {
-            throw new IllegalArgumentException("선택한 id의 메모는 존재하지 않습니다.");
-        }
+        Memo foundmemo = getMemoByID(id);
+        // 메모 내용 수정
+        foundmemo.update(memoRequestDto);
+        return id;
     }
 
     public Long deleteMemo(@PathVariable Long id) {
         // 해당 id의 메모가 존재하는지 확인
-        Memo foundmemo = MemoRepository.findById(id);
-
+        Memo foundmemo = getMemoByID(id);
         // 메모 내용 삭제
-        if (foundmemo != null){
-            Long deletedID = MemoRepository.delete(id);
-            return deletedID;
-
-        } else {
-            throw new IllegalArgumentException("선택한 id의 메모는 존재하지 않습니다.");
-        }
+        memoRepository.delete(foundmemo);
+        return id;
     }
 
 }
